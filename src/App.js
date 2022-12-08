53aa3eb43b3a509efaa27d331b881567d48de1e4
Sdev237 <sdevsap@gmail.com>
2022-12-08 13:58:50 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import  Login  from './pages/Login';
 import SignUp from './pages/SignUp';
 import Footer from './component/nav/Footer'
@@ -7,24 +7,21 @@ import ForgotPaswordEmail from './pages/ForgotPaswordEmail'
 import ResetPassword from './pages/ResetPassword'
 import StepTwoSignUp from './pages/StepTwoSignUp';
 import SignUpPage from './pages/registration/SignUpPage'
-// import './App.css';
+import Acceuil from './Acceuil';
+import './App.css';
 
 
   function App() {
 
    
     return (
-      <div>
-        <Routes>
-          <Route path="/login" element={<Login/> } />
-          <Route path="/Sign-up" element={<SignUp/> } />
-          <Route path="/forgot-password" element={<ForgotPaswordEmail/> } />
-          <Route path="/reset-password" element={<ResetPassword/> } />
-          <Route path="/step-two" element={<StepTwoSignUp/> } />
-          <Route path="/step-tree" element={<SignUpPage/> } />
-        </Routes>
-        <Footer/> 
-      </div>
+      <Router>
+        <div className='App'>
+          <Routes>
+            <Route path="/" element={<Acceuil /> } />
+          </Routes>
+        </div> 
+      </Router>
     );
   }
 
